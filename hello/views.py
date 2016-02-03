d81ad43e65f7308a92caef87f17147dc4a526cc5
Lucas Swartsenburg <lucas@optimizely.com>
2016-02-02 18:07:36 -0800

    working 3rd party cookie pixel example

--- hello/views.py
+++ hello/views.py
@@ -1,11 +1,11 @@
-from django import http
 from django.http import HttpResponse
 import json
 import random
 import string
 from datetime import datetime
 
-domain = '.third-party-return.appspot.com'
+DOMAIN = '.third-party-return.appspot.com'
+COOKIENAME = 'otid'
 
 def json_response(func):
     """
@@ -14,13 +14,8 @@ def json_response(func):
     the response is JSONP.
     """
     def decorator(request, *args, **kwargs):
-        # N = 10
-        # expires = datetime.now().replace(year=datetime.now().year + 10)
-        # print request.COOKIES
-        # request.COOKIES['otid'] = (request.COOKIES['otid'] if 'otid' in request.COOKIES else ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N)))
         objects = func(request, *args, **kwargs)
         if isinstance(objects, HttpResponse):
-            # objects.set_cookie('otid', request.COOKIES['otid'], domain=domain, expires=expires)
             return objects
         try:
             data = json.dumps(objects)
@@ -28,23 +23,25 @@ def json_response(func):
                 # a jsonp response!
                 data = '%s(%s);' % (request.REQUEST['callback'], data)
                 response = HttpResponse(data, "text/javascript")
-                # response.set_cookie('otid', request.COOKIES['otid'], domain=domain, expires=expires)
                 return response
         except:
             data = json.dumps(str(objects))
         response = HttpResponse(data, "application/json")
-        # response.set_cookie('otid', request.COOKIES['otid'], domain=domain, expires=expires)
         return response
     return decorator
 
-def pixel(func):
+def pixel(request):
     response = HttpResponse({}, "image/png")
-    N = 10
-    expires = datetime.now().replace(year=datetime.now().year + 10)
-    ran = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N))
-    response.set_cookie('otid', ran, domain=domain, expires=expires)
+    add_id_if_not_set(request, response)
     return response
     
+def add_id_if_not_set(request, response):
+    if COOKIENAME not in request.COOKIES:
+        N = 10
+        expires = datetime.now().replace(year=datetime.now().year + 10)
+        ran = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N))
+        response.set_cookie(COOKIENAME, ran, domain=DOMAIN, expires=expires)
+    return response
 
 @json_response
 def home(request):
